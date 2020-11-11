ce1524a3f3af01f0bbf53c18409eea6b95345991
mirceachira <chira.mircea.darius@gmail.com>
2020-11-11 13:42:01 +0200

    refactor web_api app and add user registration view

--- backend/web_api/models.py
+++ backend/web_api/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserLocation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    lat = models.IntegerField()
+    lng = models.IntegerField()
+
+
+
+from django.db import models
 
 
 class HelloWorld(models.Model):
     id = models.IntegerField(primary_key=True)
     hello = models.CharField(max_length=10)
     count = models.IntegerField()
-    
+
     def __str__(self):
         return self.hello
 
--- backend/web_api/urls.py
+++ backend/web_api/urls.py
@@ -1,13 +1,10 @@
-from rest_framework_simplejwt import views as jwt_views
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
-from web_api.views import HelloWorldView
+from web_api.views import HelloWorldView, RegisterApi
 
 
 urlpatterns = [
-    # path('token/obtain/', ObtainTokenPairWithColorView.as_view(), name='token_obtain'),
-    # path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    # path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
-
     path('hello_world/', HelloWorldView.as_view()),
+    path('register/', RegisterApi.as_view()),
 ]
