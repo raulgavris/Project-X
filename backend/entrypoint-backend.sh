b43e0763c0ba0d27caa15de2fd0514b95a4ea2d8
Raul Gavris <rg.raulgavris@gmail.com>
2020-11-21 15:19:24 +0200

    fixed docker-compose

--- backend/entrypoint-backend.sh
+++ backend/entrypoint-backend.sh
@@ -4,5 +4,6 @@ sleep 30
 python3 manage.py collectstatic
 python3 manage.py makemigrations
 python3 manage.py migrate --run-syncdb
+python3 manage.py migrate --run-syncdb
 
 gunicorn --bind 0.0.0.0:8000 --log-level=debug main.wsgi
